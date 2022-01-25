c716b8d413e5438a3f8f5b692c0afd9995fb70ae
705H-S <93358442+705h-S@users.noreply.github.com>
2022-01-24 17:31:40 -0800

    License function will return certain text if user inputes NONE

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license !== "NONE")
+    if (license !== "NONE"){
+    return `![License](https://img.shields.io/badge/license-${license}-orange)`;
+}
+return ""
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license !== "NONE")
+    if (license !== "NONE"){
+        return `* [License](#License)`;
+    }
+    return ""
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    if (license !== "NONE")
+    if (license !== "NONE"){
+        return `## License
+        
+        Copyright © ${license}. All rights reserved. `;
+    }
+    return ""
 }
 
 // TODO: Create a function to generate markdown for README

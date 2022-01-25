24a22db9648f4c339181bad0c30a38de0b65eff4
705H-S <93358442+705h-S@users.noreply.github.com>
2022-01-24 16:53:53 -0800

    added questions for user to answer

---
+++ index.js
@@ -0,0 +1,52 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Whats your project? What does it do?"
+    },
+
+    {
+        type: "input",
+        name: "install",
+        message: "What are the steps required to install your project?"
+    },
+
+    {
+
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["Apache 2.0", "Boost 1.0", "BSD 2", "BSD 3", "GNPLv3", "IBM", "ISC", "MIT", "NONE"]
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use your project?"
+    },
+
+    {
+        type: "input",
+        name: "GitHub",
+        message: "Enter your GitHub user name"
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
